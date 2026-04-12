fd81ef8dfd063316c3422c27b869d406a5795b71
Dmytro Afonin <stealss7@gmail.com>
2026-04-13 00:47:30 +0200

    Stealss7/lab 44 initialize convex project and astro integration (#10)
    * design ui
    
    * cleanup
    
    * add subscribe logic
    
    * cleanup
    
    * fix DB and env name cosistency
    
    * handle error properly
    
    * simplify theming, remove duplications
    
    * fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,4 +1,5 @@
-import { siteConfig } from "../config/site";
+import { siteConfig, absoluteUrl } from "../config/site";
+import { getPostUrl, getVisiblePosts } from "../utils/posts";
 
 export const prerender = true;
 
@@ -11,14 +12,35 @@ function escapeXml(value: string) {
     .replaceAll("'", "&apos;");
 }
 
-export function GET() {
+export async function GET() {
+  const posts = await getVisiblePosts();
+  const lastBuildDate = new Date().toUTCString();
+
+  const itemsXml = posts
+    .map((post) => {
+      const link = absoluteUrl(getPostUrl(post));
+      const pubDate = post.data.pubDate.toUTCString();
+
+      return `    <item>
+      <title>${escapeXml(post.data.title)}</title>
+      <link>${escapeXml(link)}</link>
+      <guid isPermaLink="true">${escapeXml(link)}</guid>
+      <pubDate>${escapeXml(pubDate)}</pubDate>
+      <description>${escapeXml(post.data.description)}</description>
+    </item>`;
+    })
+    .join("\n");
+
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>${escapeXml(siteConfig.title)}</title>
     <description>${escapeXml(siteConfig.description)}</description>
     <link>${escapeXml(siteConfig.url)}</link>
     <language>${escapeXml(siteConfig.lang)}</language>
+    <lastBuildDate>${escapeXml(lastBuildDate)}</lastBuildDate>
+    <atom:link href="${escapeXml(absoluteUrl("/rss.xml"))}" rel="self" type="application/rss+xml" />
+${itemsXml}
   </channel>
 </rss>`;
 
